80a01496e3605fa58d426ad1cc87a6e018549a12
Valerio Sevilla <valerio.sevilla@gmail.com>
2017-04-22 18:44:43 +0200

    ADD Camera movement

---
+++ Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject target;
+
+	private Vector3 cameraOffset;
+
+	void Awake () {
+		cameraOffset = transform.position - target.transform.position;
+	}
+
+	void Update () {
+		Quaternion _targetRotation = target.transform.rotation;
+		Quaternion _rotation = Quaternion.AngleAxis (_targetRotation.eulerAngles.z, Vector3.back);
+		transform.rotation = _rotation;
+
+		transform.position = target.transform.position + (_rotation * cameraOffset);
+	}
+}
