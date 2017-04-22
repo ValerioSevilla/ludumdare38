88f214309dc12919217ff57272850d64cda72323
Valerio Sevilla <vsevilla@financialforce.com>
2017-04-22 13:26:05 +0200

    ADD Planet, character and gravity

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	private static class Constants {
+		public const float GRAVITY_MAGNITUDE = -9.8f;
+	}
+
+	private GameObject planet;
+	private Rigidbody2D rigidBody;
+
+	void Awake () {
+		planet = GameObject.Find ("Planet");
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 _direction = transform.position - planet.transform.position;
+		Vector2 _gravity = new Vector2 (_direction.x, _direction.y).normalized * Constants.GRAVITY_MAGNITUDE;
+
+		rigidBody.AddForce (_gravity);
+		transform.rotation = Quaternion.LookRotation(Vector3.back, _direction);
+	}
+}
