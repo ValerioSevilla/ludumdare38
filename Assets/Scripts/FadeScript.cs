318b0ef9ca3ca740246c80d2c64932a2a9d4ca9f
Valerio Sevilla <valerio.sevilla@gmail.com>
2017-04-24 00:35:47 +0200

    ADD Game Over

---
+++ Assets/Scripts/FadeScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeScript : MonoBehaviour {
+
+	private int FADEOUT_TRIGGER_HASH = Animator.StringToHash("FadeOut");
+
+	private Animator anim;
+	private string nextSceneName;
+
+	public void fadeOut (string _nextSceneName) {
+		anim.SetTrigger (FADEOUT_TRIGGER_HASH);
+
+		nextSceneName = _nextSceneName;
+	}
+
+	public void commitFadeOut () {
+		SceneManager.LoadScene (nextSceneName);
+	}
+
+	void Awake () {
+		anim = GetComponent<Animator> ();
+	}
+}
