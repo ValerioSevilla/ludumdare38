7510b0be86cb1c936102d4e76578c230dd07070a
Valerio Sevilla <valerio.sevilla@gmail.com>
2017-04-24 02:06:24 +0200

    ADD Win scene

---
+++ Assets/Scripts/WinSceneWait.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinSceneWait : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds (3.0f);
+
+		GameObject.Find ("Canvas/Fade").GetComponent<FadeScript> ().fadeOut ("Main");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
